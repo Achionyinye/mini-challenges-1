af70faadb8064ce39322c8497b642b85aa1e2340
Decagon <decagon@Decagons-MacBook-Air.local>
2022-01-31 11:05:22 +0100

    my first challenge done

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,53 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+// function isValid(str) {
+//     let brackets = "[]{}()<>"
+//     let store = []
+
+//     for (let bracket of str) {
+//         let indexOfBracket = brackets.indexOf(bracket)
+
+//         if (indexOfBracket % 2 === 0) {
+//             store.push(indexOfBracket + 1)
+//         } else {
+//             if (stack.pop() !== bracketsIndex) {
+//                 return invalid;
+//             }
+//         }
+//     }
+//     return stack.length === 0
+// }
+
+
+function isValid(str) {
+    const openers = "[{(";
+
+
+    obj = {
+        '}': '{',
+        ']': '[',
+        ')': '('
+    }
+
+    const stack = [];
+    for (let i = 0; i < str.length; i++) {
+        if (openers.includes(str[i])) {
+            stack.push(str[i])
+        } else {
+            let last = stack[stack.length - 1];
+            if (obj[str[i]] === last) {
+                stack.pop()
+            }
+
+        }
+    }
+
+
+    return stack.length ? 'invalid' : 'valid'
+}
+
+console.log(isValid(`[(]))`))
+
 
 module.exports = isValid;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,40 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    const newM = array[0].length//5
+    const newN = array.length//3
+    let newArray = []
+
+
+    //turn an m x n array into a n x m array
+    for (let i = 0; i < newM; i++) {
+        let newEntry = []
+        for (let j = 0; j < newN; j++) {
+            newEntry.push(array[j][i])
+        }
+        newArray[i] = [...newEntry]
+    }
+    return newArray
+}
+
+console.log(transpose([
+    [5, 12, 17, 9, 3],
+  [13, 4, 8, 14, 1],
+  [9, 6, 3, 7, 21],
+]))
+
+
+
+//m =vertical(3) ,n= horizontal(5)
+
+// [
+//     [5, 13, 9],
+//     [12, 4, 6],
+//     [17, 8, 3],
+//     [9, 14, 7],
+//     [3, 1, 21],
+//   ];
+//m =vertical(5) ,n= horizontal(3)
 
 module.exports = transpose;
